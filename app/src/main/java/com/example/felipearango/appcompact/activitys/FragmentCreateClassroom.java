880ee4f5e3683ecb5e212334ce0d90c7f7e0a5d0
sanmartin98 <camilo9856@hotmail.com>
2017-12-09 15:53:27 -0500

    Creacion de aulas

--- app/src/main/java/com/example/felipearango/appcompact/activitys/FragmentCreateClassroom.java
+++ app/src/main/java/com/example/felipearango/appcompact/activitys/FragmentCreateClassroom.java
@@ -1,28 +1,35 @@
 package com.example.felipearango.appcompact.activitys;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.felipearango.appcompact.R;
+import com.example.felipearango.appcompact.models.ManejoUser;
+import com.example.felipearango.appcompact.models.Util;
 
 
 public class FragmentCreateClassroom extends Fragment implements View.OnClickListener{
 
     Button btnPublicarAula;
     View view;
+    EditText txtNomAula, txtDesAula;
+    public static String nombre_aula = "";
+    public static String descripcion_aula = "";
+    public static String key_aula = "";
+    private ManejoUser mn = new ManejoUser();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_create_classroom, container, false);
-        btnPublicarAula = view.findViewById(R.id.btnPublicarAula);
+        initComponents();
+
         btnPublicarAula.setOnClickListener(this);
         return view;
     }
@@ -32,14 +39,28 @@ public class FragmentCreateClassroom extends Fragment implements View.OnClickLis
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.btnPublicarAula:{
-
-                Activity_Principal activity = (Activity_Principal)view.getContext();
-                FragmentClassroomCode fragmentClassroomCode = new FragmentClassroomCode();
-                activity.getSupportFragmentManager().beginTransaction().replace(R.id.FrFragments, fragmentClassroomCode).addToBackStack(null).commit();
-
-
+                if(validarCampos()) {
+                    nombre_aula = txtNomAula.getText().toString();
+                    descripcion_aula = txtDesAula.getText().toString();
+                    key_aula = mn.databaseReference.push().getKey();
+                    Activity_Principal activity = (Activity_Principal) view.getContext();
+                    FragmentClassroomCode fragmentClassroomCode = new FragmentClassroomCode();
+                    activity.getSupportFragmentManager().beginTransaction().replace(R.id.FrFragments, fragmentClassroomCode).addToBackStack(null).commit();
+                }
                 break;
             }
         }
+    }
+
+    private void initComponents(){
+        btnPublicarAula = view.findViewById(R.id.btnPublicarAula);
+        txtNomAula = view.findViewById(R.id.txtNombreAula);
+        txtDesAula = view.findViewById(R.id.txtDescripcionAula);
+        mn.inicializatedFireBase();
+    }
+
+    private boolean validarCampos(){
+        return !Util.emptyCampMSG(txtNomAula,"Por favor ingrese el nombre del aula") &&
+                !Util.emptyCampMSG(txtDesAula,"Por favor ingrese una descripcion del aula");
     }
 }
