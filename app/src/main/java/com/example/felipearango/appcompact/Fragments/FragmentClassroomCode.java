c4e16bdb37f83a6fa37bf89cc1e39bacb2a5b148
Sebassllr <Sebaslunasllr@gmail.com>
2018-03-29 17:54:17 -0500

    Se organizan aulas para que tenga el botón que se activa cuando el usuario es un profesor.

--- app/src/main/java/com/example/felipearango/appcompact/Fragments/FragmentClassroomCode.java
+++ app/src/main/java/com/example/felipearango/appcompact/Fragments/FragmentClassroomCode.java
@@ -1,7 +1,7 @@
 package com.example.felipearango.appcompact.Fragments;
 
 import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -24,6 +24,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class FragmentClassroomCode extends Fragment implements View.OnClickListener{
@@ -37,15 +38,18 @@ public class FragmentClassroomCode extends Fragment implements View.OnClickListe
     private EditText etEmail;
     private TextView tvKey;
     private ManejoUser mn = new ManejoUser();
-    final ArrayList<Usuario_estudiante> lstUser = new ArrayList<>();
-    final ArrayList<String> lstEstudiantes = new ArrayList<>();
+    private Bundle args;
+    private ArrayList<Usuario_estudiante> lstUser = new ArrayList<>();
+    private ArrayList<String> lstEstudiantes = new ArrayList<>();
+    private Boolean datos;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view =  inflater.inflate(R.layout.fragment_classroom_code, container, false);
-        setRecycler();
+
         initComponents();
         initListStudents();
+        setRecycler();
         return view;
     }
 
@@ -59,17 +63,28 @@ public class FragmentClassroomCode extends Fragment implements View.OnClickListe
         recycler.setLayoutManager(lManager);
 
         // Crear un nuevo adaptador
-        adapter = new RecyclerAdapterAddStudent(view.getContext(), mData);
+        adapter = new RecyclerAdapterAddStudent(view.getContext(), mData.size() == 0 ? mData : convertToStudents(mData));
         recycler.setAdapter(adapter);
     }
 
     private void initComponents(){
+
         etEmail = view.findViewById(R.id.etEmail);
         btnAdd = view.findViewById(R.id.btnAdd);
         btnPublicarAula = view.findViewById(R.id.btnPublicarAula);
         tvKey = view.findViewById(R.id.tvKeyAula);
         mn.inicializatedFireBase();
-        tvKey.setText(FragmentCreateClassroom.key_aula);
+        args = getArguments();
+        if(args != null){
+            datos = args.getBoolean("datos", Boolean.FALSE);
+            if(datos != null && datos){
+                lstEstudiantes = args.getStringArrayList("integrantes");
+                tvKey.setText(args.getString("key"));
+            }
+        }else{
+            tvKey.setText(FragmentCreateClassroom.key_aula);
+        }
+
         btnAdd.setOnClickListener(this);
         btnPublicarAula.setOnClickListener(this);
     }
@@ -120,6 +135,16 @@ public class FragmentClassroomCode extends Fragment implements View.OnClickListe
                     Usuario_estudiante u = user.getValue(Usuario_estudiante.class);
                     lstUser.add(u);
                 }
+                if(datos != null && datos){
+                    ArrayList<String> data = convertToStudents(lstEstudiantes);
+                    for (int i = 0; i < data.size(); i++) {
+                        int position = 0;
+                        mData.add(position, data.get(i));
+                        adapter.notifyItemInserted(position);
+                        adapter.notifyDataSetChanged();
+                        recycler.scrollToPosition(position);
+                    }
+                }
             }
             @Override
             public void onCancelled(DatabaseError databaseError) {
@@ -142,5 +167,17 @@ public class FragmentClassroomCode extends Fragment implements View.OnClickListe
             }
         }
         return false;
+    }
+
+    private ArrayList<String> convertToStudents(ArrayList<String> lstEstudiantesIds){
+        ArrayList<String> userEmails = new ArrayList<>();
+        for (String stdid : lstEstudiantesIds) {
+            for (Usuario_estudiante user: lstUser) {
+                if(user.getUid().equals(stdid)){
+                    userEmails.add(user.getCorreo());
+                }
+            }
+        }
+        return userEmails;
     }
 }
